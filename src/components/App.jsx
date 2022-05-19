e9cb5819b3bfa8a6bd9d93cab04081ffbb393437
nik-349 <70761612+nik-349@users.noreply.github.com>
2022-05-19 20:52:18 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,9 @@
-import ProfileList from './Profile/ProfileList';
-import StatisticsList from './Statistics/StatisticsList';
-import FriendsList from './FriendList/FriendList';
-import TransactionList from './TransactionHistory/TransactionList';
 
-import user from '../../src/user.json';
-import data from '../../src/data.json';
-import friends from '../../src/friends.json';
-import transactions from '../../src/transactions.json';
 
 export const App = () => {
   return (
     <div className="container">
-      <ProfileList item={user} />
-      <FriendsList items={friends} />
-      <StatisticsList title="Upload stats" stats={data} />
-      <TransactionList items={transactions} />
+www
     </div>
   );
 };
